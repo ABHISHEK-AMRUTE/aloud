16c5be4ad0d9feb632f7d7f163bd1a6a808794c6
NiharikaNishu <niharikanishu2941@gmail.com>
2023-07-14 00:35:32 +0530

    add sql query

--- hello.py
+++ hello.py
@@ -20,6 +20,8 @@ class hello:
         @self.app.route('/openai', methods=['POST'])
         def openai_api():
             data = json.loads(request.data)
+            print(data['requests']['objective'])
+
             response = openai.ChatCompletion.create(
                 engine="dorkupinetreeGPT35",
                 messages=[
@@ -28,11 +30,14 @@ class hello:
                 "2.Objective : " + data['requests']['details'] + "\n" +
                 "3. metrics : [{\"ObyVi\" : \"This means total users who've placed an order divided by (total users who visited the platform)},{\"AOV\":\"This is the average sale made by users on the platform\"},{\"Cancellation\" : \"Users who cancel an order\"},{\"Return rate\" : \"Users who've returned an order\"},{\"Seller_exp\" : \"Bad seller experience\"},{\"First time app open date\" : \"When did the user first came to the platform?\"}]"},
                     {"role": "user",
-                     "content": "provide the only metrics that will be closely impacted for the given details and objective from list of metrics provided. Also provide set of minimum 5 very very simple questions that must be asked to the customer of my e-commerce platform. The questions that I want to ask should just be around the details and objective that I've mentioned above (and nothing generic) Remember that my customers are extremely not tech savvy. PLease make simple questions that are easy for the user but important for me. Adapt the response in below format and add questions in key lod_deeds {\n" +
+                     "content": "provide the only metrics that will be closely impacted for the given details and objective from list of metrics provided. Also provide set of minimum 5 very very simple questions that must be asked to the customer of my e-commerce platform. The questions that I want to ask should just be around the details and objective that I've mentioned above (and nothing generic) Remember that my customers are extremely not tech savvy. PLease make simple questions that are easy for the user but important for me. Adapt the response in below format and add questions in key lod_deeds of response format"
+                     "{\"Response\": \n"
+                                " {\n" +
                                 "    \"metrics\": [\n" +
                                 "        {\n" +
                                 "            \"name\": \"\",\n" +
                                 "            \"domain\": \"\"\n" +
+                                "            \"sql_query\": \"\"\n" +
                                 "        }\n" +
                                 "    ],\n" +
                                 "    \"lod_deeds\": [\n" +
@@ -42,7 +47,7 @@ class hello:
                                 "} "},
 
                 ],
-                temperature=0.5,
+                temperature=0.2,
                 max_tokens=800,
                 top_p=0.95,
                 frequency_penalty=0,
@@ -51,9 +56,10 @@ class hello:
             )
             print(response)
             ct = response.choices[0].message.content;
+            print(ct)
             return ct
 
-    def run(self, host='0.0.0.0', port=5099):
+    def run(self, host='0.0.0.0', port=5031):
         self.app.run(host=host, port=port)
 
 if __name__ == '__main__':
