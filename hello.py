cef86f8b4afb280edd3ff1b7e52005263ed96684
NiharikaNishu <niharikanishu2941@gmail.com>
2023-07-13 17:03:00 +0530

    dummy commit

--- hello.py
+++ hello.py
@@ -1,7 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify
+import openai
 
-app = Flask(__name__)
+class AutoPrompt:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+        # Set OpenAI API credentials
+        openai.api_key = 'e20507358b0042cdb8cc04f70b0311ba'
+        openai.api_type = "azure"
+        openai.api_base = "https://hackmee2.openai.azure.com/"
+        openai.api_version = "2023-03-15-preview"
+
+        # Route for health API
+        @self.app.route('/health', methods=['GET'])
+        def health():
+            return jsonify({'status': 'OK'})
+
+        # Route for OpenAI API integration
+        @self.app.route('/openai', methods=['GET'])
+        def openai_api():
+            response = openai.ChatCompletion.create(
+                engine="dorkupinetreeGPT35",
+                messages=[
+                    {"role": "system", "content": "You are an AI assistant that helps people find information."},
+                    {"role": "user", "content": "How are you"},
+                    {"role": "assistant",
+                     "content": "As an AI language model, I do not have feelings or emotions. However, I am functioning well and ready to assist you with any queries or tasks you may have. How can I assist you today?"}
+                ],
+                temperature=0.7,
+                max_tokens=800,
+                top_p=0.95,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop=None
+            )
+            return response
+
+    def run(self, host='0.0.0.0', port=5011):
+        self.app.run(host=host, port=port)
+
+if __name__ == '__main__':
+    my_app = AutoPrompt()
+    my_app.run()
