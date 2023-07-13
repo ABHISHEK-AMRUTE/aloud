0ba296ca2103ccd100830d4ddd1190dc764bdc21
ABHISHEK-AMRUTE <abhishekamrutelfs@gmail.com>
2023-07-13 21:26:50 +0530

    response changes

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, json
 import openai
 
 class hello:
@@ -19,12 +19,13 @@ class hello:
         # Route for OpenAI API integration
         @self.app.route('/openai', methods=['POST'])
         def openai_api():
+            data = json.loads(request.data)
             response = openai.ChatCompletion.create(
                 engine="dorkupinetreeGPT35",
                 messages=[
                     {"role": "system", "content": "You are an analyzer and summarizer system that takes the following input : \n" +
-                "1. Details : I've run a new A/B experiment on my e-commerce platform. I'm showing different delivery dates to different users and I want to check the impact on behaviour of my users.\n" +
-                "2.Objective : There are three variants that I'm testing. Control group to which I show same delivery date as the platform, test-1 for which I've reduced 1 delivery day and for test-2 I've reduced 2 delivery dates. But what is constant is the actual delivery date. Now I want to understand  how does changing perception of delivery date impact the users' buying behaviour.\n" +
+                "1. Details : "+ data['requests']['objective'] +"\n" +
+                "2.Objective : " + data['requests']['details'] + "\n" +
                 "3. metrics : [{\"ObyVi\" : \"This means total users who've placed an order divided by (total users who visited the platform)},{\"AOV\":\"This is the average sale made by users on the platform\"},{\"Cancellation\" : \"Users who cancel an order\"},{\"Return rate\" : \"Users who've returned an order\"},{\"Seller_exp\" : \"Bad seller experience\"},{\"First time app open date\" : \"When did the user first came to the platform?\"}]"},
                     {"role": "user", "content": "identify the metrics among the given metrics in this message that will be suitable for the given objective and details"},
 
