5797abdb27a496ef93dc840f691f15d6e35ca1a5
tarun-198 <tarundeep.singh@meesho.com>
2023-07-14 01:26:17 +0530

    temp

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request, json
 import openai
+import sqlite3
 
 class hello:
     def __init__(self):
         self.app = Flask(__name__)
-
+        self.initialise_DB()
         # Set OpenAI API credentials
         openai.api_key = 'e20507358b0042cdb8cc04f70b0311ba'
         openai.api_type = "azure"
@@ -58,6 +59,104 @@ class hello:
             print(response)
             ct = response.choices[0].message.content
             return ct
+
+        @self.app.route('/summarizer', methods=['POST'])
+        def summarizer_api():
+
+            business_feature = "give your feature"
+
+            data = json.loads(request.data)
+            type_of_summary = data['requests']['type_of_summary'];
+            cohort = data['requests']['cohort'];
+
+            con = sqlite3.connect("tutorial.db")
+            cur = con.cursor()
+            res = cur.execute("SELECT Q1,Q2,Q3,Q4,Q5,Q6,Q7 FROM qna WHERE ")
+            query = res.fetchall()
+
+            summaries = []
+
+            for i in query:
+                message = [
+                    {"role": "system", "content": business_feature},
+                    {"role": "assistant", "content": Q1},
+                    {"role": "user", "content": i[0]},
+                    {"role": "assistant", "content": Q2},
+                    {"role": "user", "content": i[1]},
+                    {"role": "assistant", "content": Q3},
+                    {"role": "user", "content": i[2]},
+                    {"role": "assistant", "content": Q4},
+                    {"role": "user", "content": i[3]},
+                    {"role": "assistant", "content": Q5},
+                    {"role": "user", "content": i[4]},
+                    {"role": "assistant", "content": Q6},
+                    {"role": "user", "content": i[5]},
+                    {"role": "assistant", "content": Q7},
+                    {"role": "user", "content": i[6]},
+                    {"role": "system", "content": "Summarize"}
+                ]
+
+                # Call OpenAI API
+                response = openai.ChatCompletion.create(
+                    engine="dorkupinetreeGPT35",
+                    messages=messages,
+                    temperature=0.2,
+                    max_tokens=1200,
+                    top_p=0.95,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    stop=None
+                )
+
+                summaries = summaries + [response.choices[0]["message"]["content"]]
+
+            combined_summary = ""
+
+            for s in summaries:
+                combined_summary = combined_summary + s + "\n"
+
+            response = openai.ChatCompletion.create(
+                engine="dorkupinetreeGPT35",
+                messages=[
+                    {"role": "system",
+                     "content": "There are three variants that I'm testing. Control group to which I show same delivery date as the platform, test-1 for which I've reduced 1 delivery day and for test-2 I've reduced 2 delivery dates. But what is constant is the actual delivery date. Now I want to understand  how does changing perception of delivery date impact the users' buying behaviour."},
+                    {"role": "user", "content": combined_summary},
+                    {"role": "system",
+                     "content": "Summarize the above conversation between assistant and user with respect to the content provided earlier to the system. Also provide a separate section of recomendataion on what can be improved in the feature"}
+                ],
+                temperature=0,
+                max_tokens=800,
+                top_p=0.95,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop=None)
+
+            return response
+
+
+    def run(self, host='0.0.0.0', port=5039):
+        self.app.run(host=host, port=port)
+
+
+    def initialise_DB(self):
+        con = sqlite3.connect("tutorial.db")
+        cur = con.cursor()
+        cur.execute(
+            "CREATE TABLE qna (User_ID,Gender,Tier,Legacy,User Type,App Opens,Orders placed,Dispatched,Delivered,Returned,GMV,Q1,Q2,Q3,Q4,Q5,Q6,Q7)")
+
+        cur.execute("""
+                INSERT INTO qna VALUES (1, 'Male', 'Gold', 'No', 'New User', 12, 4, 4, 4, 0, 50, 'Yes', 'No', 4, 5, 'I prefer faster delivery as it affects my decision to purchase.', 'No, I don''t return the order even if it''s delivered late.', 'No, I don''t cancel my orders if the delivery date is high for me. I wait for the delivery.'),
+        (2, 'Female', 'Silver', 'No', 'Returning User', 20, 8, 8, 7, 1, 120.5, 'Yes', 'Yes', 3, 4, 'Delivery date plays a minor role in my buying decision.', 'Yes, if the order is significantly delayed, I might consider returning it.', 'No, I rarely cancel orders based on delivery date.'),
+        (3, 'Male', 'Bronze', 'Yes', 'New User', 5, 2, 1, 1, 0, 15.2, 'No', 'No', 2, 3, 'Delivery date doesn''t matter much to me.', 'No, I usually keep the order even if it''s late.', 'Yes, I cancel my orders if the delivery date is too high for me.'),
+        (4, 'Female', 'Gold', 'No', 'Returning User', 32, 12, 12, 12, 0, 200.8, 'Yes', 'No', 5, 5, 'Delivery date is crucial for my purchasing decisions.', 'No, I don''t return the order even if it''s delivered late.', 'No, I don''t cancel my orders based on delivery date.'),
+        (5, 'Male', 'Silver', 'Yes', 'New User', 8, 3, 3, 3, 0, 40, 'Yes', 'No', 4, 4, 'Delivery date has some influence on my buying behavior.', 'No, I don''t return the order even if it''s delivered late.', 'No, I rarely cancel orders due to delivery date.'),
+        (6, 'Male', 'Bronze', 'No', 'New User', 10, 5, 4, 4, 1, 60.75, 'Yes', 'Yes', 3, 3, 'Delivery date doesn''t affect my buying decisions much.', 'Yes, if the order is significantly delayed, I might consider returning it.', 'No, I rarely cancel orders based on delivery date.'),
+        (7, 'Female', 'Gold', 'No', 'Returning User', 28, 10, 10, 10, 0, 180.9, 'Yes', 'No', 5, 5, 'Delivery date is essential for me to make a purchase.', 'No, I don''t return the order even if it''s delivered late.', 'No, I don''t cancel my orders based on delivery date.'),
+        (8, 'Male', 'Silver', 'Yes', 'New User', 15, 6, 6, 6, 0, 95.5, 'Yes', 'No', 4, 4, 'Delivery date plays a moderate role in my buying decision.', 'No, I don''t return the order even if it''s delivered late.', 'No, I rarely cancel orders based on delivery date.'),
+        (9, 'Female', 'Bronze', 'Yes', 'Returning User', 18, 9, 9, 9, 1, 110.25, 'Yes', 'Yes', 3, 4, 'Delivery date slightly influences my purchasing decisions.', 'Yes, if the order is significantly delayed, I might consider returning it.', 'No, I rarely cancel orders due to delivery date.'),
+        (10, 'Male', 'Gold', 'No', 'New User', 25, 11, 11, 11, 0, 160, 'Yes', 'No', 5, 5, 'Delivery date is a critical factor in my buying behavior.', 'No, I don''t return the order even if it''s delivered late.', 'No, I don''t cancel my orders based on delivery date.')
+                """)
+
 
     def run(self, host='0.0.0.0', port=5031):
         self.app.run(host=host, port=port)
