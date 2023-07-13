15c02b25484632639a658820cf72c8ab3a837694
NiharikaNishu <niharikanishu2941@gmail.com>
2023-07-13 20:44:28 +0530

    commit

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import openai
 
+
 class hello:
     def __init__(self):
         self.app = Flask(__name__)
@@ -22,14 +23,22 @@ class hello:
             response = openai.ChatCompletion.create(
                 engine="dorkupinetreeGPT35",
                 messages=[
-                    {"role": "system", "content": "You are an analyzer and summarizer system that takes the following input : \n" +
-                "1. Details : I've run a new A/B experiment on my e-commerce platform. I'm showing different delivery dates to different users and I want to check the impact on behaviour of my users.\n" +
-                "2.Objective : There are three variants that I'm testing. Control group to which I show same delivery date as the platform, test-1 for which I've reduced 1 delivery day and for test-2 I've reduced 2 delivery dates. But what is constant is the actual delivery date. Now I want to understand  how does changing perception of delivery date impact the users' buying behaviour.\n" +
-                "3. metrics : [{\"ObyVi\" : \"This means total users who've placed an order divided by (total users who visited the platform)},{\"AOV\":\"This is the average sale made by users on the platform\"},{\"Cancellation\" : \"Users who cancel an order\"},{\"Return rate\" : \"Users who've returned an order\"},{\"Seller_exp\" : \"Bad seller experience\"},{\"First time app open date\" : \"When did the user first came to the platform?\"}]"},
-                    {"role": "user", "content": "identify the metrics among the given metrics in this message that will be suitable for the given objective and details"},
+                    {"role": "system", "content": "You are an analyzer system that takes the following input : \n" +
+                                                  "1. Details : I'm a product manager and I've run a new A/B experiment on my e-commerce platform. I'm showing different delivery dates to different users and I want to check the impact on behaviour of my users.\n" +
+                                                  "2.Objective : There are three variants that I'm testing. Control group to which I show same delivery date as the platform, test-1 for which I've reduced 1 delivery day and for test-2 I've reduced 2 delivery dates. But what is constant is the actual delivery date. Now I want to understand how does changing perception of delivery date impact the users' buying behaviour.\n" +
+                                                  "3. metrics : [{\"ObyVi\" : \"This means total users who've placed an order divided by (total users who visited the platform)},{\"AOV\":\"This is the average sale made by users on the platform\"},{\"Cancellation\" : \"Users who cancel an order\"},{\"Return rate\" : \"Users who've returned an order\"},{\"Seller_exp\" : \"Bad seller experience\"},{\"First time app open date\" : \"When did the user first came to the platform?\"}]"},
+                    {"role": "user",
+                     "content": "json output in below structure for the only metrics that will be closely impacted for the given details and objective from list of metrics provided\n" +
+                                "metrics : [\n" +
+                                "{\n" +
+                                "\t\"metric_name\" : \"\",\n" +
+                                "\t\"metric domain\" : \"\", //allowed values : product, technical, bussiness\n" +
+                                "\t\"sql_query\" : \"\"\n" +
+                                "}\n" +
+                                "] . Also provide set of minimum 5 very very simple questions that must be asked to the customer of my e-commerce platform. The questions that I want to ask should just be around the details and objective that I've mentioned above (and nothing generic) Remember that my customers are extremely not tech savvy. PLease make simple questions that are easy for the user but important for me "},
 
                 ],
-                temperature=0.2,
+                temperature=0.5,
                 max_tokens=800,
                 top_p=0.95,
                 frequency_penalty=0,
@@ -40,6 +49,7 @@ class hello:
 
     def run(self, host='0.0.0.0', port=5039):
         self.app.run(host=host, port=port)
+
 
 if __name__ == '__main__':
     my_app = hello()
