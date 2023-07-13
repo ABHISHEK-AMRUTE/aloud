63a7a5cd4844d7bb5f8e41d9c24cac1900455bc4
NiharikaNishu <niharikanishu2941@gmail.com>
2023-07-13 20:11:30 +0530

    POSt

--- hello.py
+++ hello.py
@@ -17,29 +17,19 @@ class hello:
             return jsonify({'status': 'OK'})
 
         # Route for OpenAI API integration
-        @self.app.route('/openai', methods=['GET'])
+        @self.app.route('/openai', methods=['POST'])
         def openai_api():
             response = openai.ChatCompletion.create(
                 engine="dorkupinetreeGPT35",
                 messages=[
                     {"role": "system", "content": "You are an analyzer and summarizer system that takes the following input : \n" +
-                "1. Details : I've run a new A/B experiment on my e-commerce platform. I'm showing different delivery dates to different users and I want to check the impact on buying behaviour of my users.\n" +
+                "1. Details : I've run a new A/B experiment on my e-commerce platform. I'm showing different delivery dates to different users and I want to check the impact on behaviour of my users.\n" +
                 "2.Objective : There are three variants that I'm testing. Control group to which I show same delivery date as the platform, test-1 for which I've reduced 1 delivery day and for test-2 I've reduced 2 delivery dates. But what is constant is the actual delivery date. Now I want to understand  how does changing perception of delivery date impact the users' buying behaviour.\n" +
-                "3. metrics : [{\"ObyVi\" : \"This means total users who've placed an order/ (total users who visited the platform)},{\"AOV\":\"This is the average sale made by users on the platform\"},{\"Cancellation\" : \"Users who cancel an order\"},{\"Return rate\" : \"Users who've returned an order\"}]"},
-                    {"role": "user", "content": "prepare the 2 responses  in sequence as mentioned based on the problem objective and details: \n" +
-                "1) json output as below\n" +
-                "metrics : [\n" +
-                "{\n" +
-                "\t\"metric_name\" : \"\",\n" +
-                "\t\"metric domain\" : \"\", //allowed values : product, technical, bussiness\n" +
-                "\t\"sql_query\" : \"\"\n" +
-                "}\n" +
-                "]\n" +
-                "\n" +
-                "2) set of minimum 5 questions that must be asked to the consumer about the feature to get its insight."},
+                "3. metrics : [{\"ObyVi\" : \"This means total users who've placed an order divided by (total users who visited the platform)},{\"AOV\":\"This is the average sale made by users on the platform\"},{\"Cancellation\" : \"Users who cancel an order\"},{\"Return rate\" : \"Users who've returned an order\"},{\"Seller_exp\" : \"Bad seller experience\"},{\"First time app open date\" : \"When did the user first came to the platform?\"}]"},
+                    {"role": "user", "content": "identify the metrics among the given metrics in this message that will be suitable for the given objective and details"},
 
                 ],
-                temperature=0.7,
+                temperature=0.2,
                 max_tokens=800,
                 top_p=0.95,
                 frequency_penalty=0,
